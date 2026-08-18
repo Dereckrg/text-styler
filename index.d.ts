fd2afedda42f3dc4ff55c56f8ecf75e8617e2453
Dereck RG <dereckrg@hotmail.com>
2026-08-18 18:17:51 -0300

    Add the types TextColor and TextMode for exportation

---
+++ index.d.ts
@@ -0,0 +1,18 @@
+export type TextColor =
+	| 'black'
+	| 'red'
+	| 'green'
+	| 'yellow'
+	| 'blue'
+	| 'magenta'
+	| 'cyan'
+	| 'white'
+	| 'gray';
+
+export type TextMode =
+	| 'bright'
+	| 'dim'
+	| 'underscore'
+	| 'blink'
+	| 'reverse'
+	| 'hidden';
