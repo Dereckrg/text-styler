153c096e745a62b9b2ebc61a76489ea59f457e4d
Dereck RG <dereckrg@hotmail.com>
2026-08-18 16:29:28 -0300

    Adds the terminalStyleReference object to the export list.

---
+++ test.js
@@ -0,0 +1,33 @@
+import textStyler from './textStyler.js';
+
+const x = textStyler.blink();
+
+console.log(textStyler.black('Black text'));
+console.log(textStyler.blue('Blue text'));
+console.log(textStyler.bright('bright text'));
+console.log(textStyler.bgBlack('bright text'));
+console.log(textStyler.bgBlue('bright text'));
+console.log(textStyler.dim('dim text'));
+console.log(textStyler.underscore('underscore text'));
+console.log(textStyler.bright('---------------------'));
+console.log(textStyler.bright().blue('Bright blue text'));
+console.log(textStyler.bright().bgBlack().blue('Bright blue text'));
+console.log(textStyler.black().underscore().bgBlack('Some text here'));
+
+const typer = textStyler.createTyper();
+typer.bright().blue('THIS IS A TYPER WIRTING').linebreak();
+typer
+	.bgBlack()
+	.gray('text text text text text text text text text text text text')
+	.reset()
+	.linebreak();
+typer
+	.bgBlack()
+	.gray('text text text text text text text text text text text text')
+	.reset()
+	.linebreak();
+typer.reset('NOw I was reseted');
+typer.bgBlue();
+typer.dim('Almost doneeeeee');
+typer.underscore().blue('Ending now');
+console.log(typer.end());
